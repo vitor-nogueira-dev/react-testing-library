5e7cd08addfab0349ac611fe2ed54d4b5e3beb69
Vitor Nogueira <vslnogueira96@gmail.com>
2023-01-24 16:35:41 -0300

    fix: ajustando pokedex

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import renderWithRouter from '../renderWithRouter';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
 describe('Testando o componente PokemonDetails', () => {
   it('', () => {
